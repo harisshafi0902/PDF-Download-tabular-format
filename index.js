2b2691e91e3389c0a4cb9a4d1b87c9e9cf24c9a0
harisshafi0902 <theharisshafi@gmail.com>
2023-05-24 12:09:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const puppeteer = require("puppeteer");
+const hbs = require("handlebars");
+const fs = require("fs-extra");
+const path = require("path");
+const data = require("./data.json");
+
+const compile = async function (templateName, data) {
+  const filePath = path.join(process.cwd(), "templates", `${templateName}.hbs`);
+  const html = await fs.readFile(filePath, "utf8");
+  return hbs.compile(html)(data);
+};
+
+(async function () {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const content = await compile("index", data);
+
+    await page.setContent(content);
+    await page.pdf({
+      path: "output.pdf",
+      format: "A4",
+      printBackground: true,
+    });
+
+    console.log("done creating pdf");
+    await browser.close();
+    process.exit();
+  } catch (e) {
+    console.log(e);
+  }
+})();
